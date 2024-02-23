20c0d7f9f4b6decdb1012e3c5c999b92fb5249e8
Ajith <kontham.ajith@gmail.com>
2024-02-23 22:47:37 +0530

    to get the paths and writing to page async

--- FirstCoreApp/Program.cs
+++ FirstCoreApp/Program.cs
@@ -1,6 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", async (HttpContext context) =>
+//{
+//    context.Response.StatusCode = StatusCodes.Status200OK;
+//    await context.Response.WriteAsync("You are in Home page");
+//});
+
+app.Run( async (HttpContext context) =>
+{
+    string path = context.Request.Path;
+    if(path == "/Home")
+    {
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        await context.Response.WriteAsync("You are in Home Page");
+    }
+    else if(path == "/Contact")
+    {
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        await context.Response.WriteAsync("You are in Contact Page");
+    }
+    else
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        await context.Response.WriteAsync("This page is not found");
+    }
+
+});
 
 app.Run();
